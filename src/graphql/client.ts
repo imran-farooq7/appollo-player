0c2598828cea3e9be84dfaf16d30f8d6bbf60c8f
imran farooq <imran_farooq7@hotmail.com>
2022-12-19 00:29:53 +0500

    Appollo client and graphql installed

---
+++ src/graphql/client.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+export const client = new ApolloClient({
+	uri: "https://evolved-phoenix-58.hasura.app/v1/graphql",
+	cache: new InMemoryCache(),
+	headers: {
+		authorization:
+			"06Z3y5OKg8Dedq97nagyvruz3U1eDLyrp8gJqz68b87aH1HirHasdUpHh5kInLBG",
+	},
+});
